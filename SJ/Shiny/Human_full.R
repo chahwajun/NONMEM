17f17c4c64a8d47bb4d7d6434a7d3e8eb9a1a6c3
chahwajun <clex9604@gmail.com>
2024-10-21 06:34:20 +0900

    human full
    human full

--- SJ/Shiny/Human_full.R
+++ SJ/Shiny/Human_full.R
@@ -5,7 +5,8 @@ library(rxode2)
 library(nonmem2rx)
 library(plotly)
 
-#model <- nonmem2rx("SG_11.ctl")  
+
+model <- nonmem2rx("SG_11.ctl")  
 
 sidebar <- card(
   textInput("dose", label = "Dose (vg/eye)",value = 100000000),
@@ -14,8 +15,7 @@ sidebar <- card(
   actionButton("simulate", label = "Simulate"),
   hr(),
     sliderInput("abline1", "Protein Concentration (ng/mL)", min = 0, max = 1000, step = 100, width = "100%", value = 800),
-    sliderInput("abline2", "Protein Concentration (ng/mL)", min = 1000, max = 5000, step = 100, width = "100%", value = 2300),
-    sliderInput("abline3", "Protein Concentration (ng/mL)", min = 5000, max = 10000, step = 1000, width = "100%", value = 7000)
+    sliderInput("abline2", "Protein Concentration (ng/mL)", min = 1000, max = 5000, step = 100, width = "100%", value = 2300)
 )
 
 abline <- card(
@@ -35,7 +35,8 @@ simulation <- navset_card_underline(
       layout_columns(
         col_widths = c(4,4,4,12),
         div(
-          tableOutput("rxode2")
+          plotlyOutput("simlationplot1"),
+          plotlyOutput("simlationplot2"),
         ),
         div(
           plotlyOutput("simlationplot3"),
@@ -90,35 +91,251 @@ server <- function(input, output, session){
   })
     
     rxsolve <- eventReactive(input$simulate, {
-      rxSolve(model, c(LNDOSE = log(as.double(input$dose)), ktr3 = 0.0000060*as.double(input$ktr3),
-                       ktr = 0.0001100*as.double(input$ktr), cl39 = 0.00000025*as.double(input$cl39),
-                       ktr5 = 0.0111000*as.double(input$ktr5), ktr2 = 0.0008550 *as.double(input$ktr2),
-                       ktr4 = 0.0023500 *as.double(input$ktr4), ktr6 = 0.0008470*as.double(input$ktr6)
+      rxSolve(model, c(LNDOSE = log(as.double(input$dose)), t.KTR3 = 0.0000060*as.double(input$ktr3),
+                       t.KTR = 0.0001100*as.double(input$ktr), t.CL39 = 0.00000025*as.double(input$cl39),
+                       t.KTR5 = 0.0111000*as.double(input$ktr5), t.KTR2 = 0.0008550 *as.double(input$ktr2),
+                       t.KTR4 = 0.0023500 *as.double(input$ktr4), t.KTR6 = 0.0008470*as.double(input$ktr6)
                        ), dosing(), nSub = 1)
     })
     
     simulation_data <- reactive({
+      results <- rxsolve()
       VIT <- results |> 
-        select(time, scale1, VIT) |> 
+        select(time, scale5, VIT) |> 
         mutate(month = time/(24*30))
-      
+      AQ <- results |> 
+        select(time, scale6, AQ) |> 
+        mutate(month = time/(24*30))
       RET <- results |> 
-        select(time, scale2, RET)|> 
+        select(time, scale7, RET)|> 
         mutate(month = time/(24*30))
-      
-      IRIS <- results |> 
-        select(time, scale4, IRIS)|> 
+      SERUM <- results |> 
+        select(time, scale8, SERUM)|> 
         mutate(month = time/(24*30))
-      
       OPT <- results |> 
-        select(time, scale3, OPT)|> 
+        select(time, scale9, OPT)|> 
         mutate(month = time/(24*30))
+      IRIS <- results |> 
+        select(time, scale10, IRIS)|> 
+        mutate(month = time/(24*30))
+      
+      list(VIT =VIT, AQ=AQ, RET=RET, SERUM=SERUM, OPT=OPT, IRIS=IRIS)
     })
     
-    output$rxode2 <- renderTable({
-      rxsolve()
+    
+    output$simlationplot1 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$VIT, aes(x = month, y = VIT/scale5), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Vitreous") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2")
+        plot <- ggplotly(plot)
     })
     
+    output$simlationplot2 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$AQ, aes(x = month, y = AQ/scale6), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Aqueous humor") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2")
+      plot <- ggplotly(plot)
+    })
+    
+    output$simlationplot3 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$RET, aes(x = month, y = RET/scale7), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Retina") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2")
+      plot <- ggplotly(plot)
+    })
+    
+    output$simlationplot4 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$SERUM, aes(x = month, y = SERUM/scale8), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Serum") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2")
+      plot <- ggplotly(plot)
+    })
+    output$simlationplot5 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$OPT, aes(x = month, y = OPT/scale9), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Optic nerve") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2")
+      plot <- ggplotly(plot)
+    })
+    
+    output$simlationplot6 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$IRIS, aes(x = month, y = IRIS/scale10), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="IRIS") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2")
+      plot <- ggplotly(plot)
+    })
+    
+    output$simlationplotlog1 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$VIT, aes(x = month, y = VIT/scale5), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Vitreous") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2") + scale_y_log10()
+      plot <- ggplotly(plot)
+    })
+    
+    output$simlationplotlog2 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$AQ, aes(x = month, y = AQ/scale6), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Aqueous humor") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        ) + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2") + scale_y_log10()
+      plot <- ggplotly(plot)
+    })
+    
+    output$simlationplotlog3 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$RET, aes(x = month, y = RET/scale7), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Retina") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) +  
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2") + scale_y_log10()
+      plot <- ggplotly(plot)
+    })
+    
+    output$simlationplotlog4 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$SERUM, aes(x = month, y = SERUM/scale8), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Serum") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2") + scale_y_log10()
+      plot <- ggplotly(plot)
+    })
+    output$simlationplotlog5 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$OPT, aes(x = month, y = OPT/scale9), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="Optic nerve") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2") + scale_y_log10()
+      plot <- ggplotly(plot)
+    })
+    
+    output$simlationplotlog6 <- renderPlotly({
+      
+      data <- simulation_data()
+      
+      plot <- ggplot() +
+        geom_line(data = data$IRIS, aes(x = month, y = IRIS/scale10), color = "#337AB7") + theme_bw() +
+        labs(x = "Time (Month)", y = "SB15 Concentration (ng/mL)", color = NULL, title="IRIS") + 
+        theme(axis.text.x = element_text(vjust = 0.5, size = 12),
+              axis.text.y = element_text(vjust = 0.5, size = 12),
+              axis.title.y = element_text(size = 14, margin = margin(t = 0, r = 20, b = 0, l = 0)),
+              axis.title.x = element_text(size = 14, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+              legend.title = element_text(size = 12)
+        )  + scale_x_continuous(breaks = seq(0, max(4,as.double(input$num)*as.double(input$interval)),12)) + 
+        geom_hline(yintercept = input$abline1, color = "yellow3") + 
+        geom_hline(yintercept = input$abline2, color = "darkorange2") + scale_y_log10()
+      plot <- ggplotly(plot)
+    })
 }
 
 
